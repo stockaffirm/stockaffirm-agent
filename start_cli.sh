08694468ea1f019da74eb2d3f8c7a7f796a915a9
pmsilcontechlabs <silcontechlabs@gmail.com>
2025-05-17 19:23:23 -0400

    changed and add start up for api and cli

--- start_cli.sh
+++ start_cli.sh
@@ -12,6 +12,6 @@ if ! pgrep -f "ollama serve" > /dev/null; then
   sleep 3
 fi
 
-# Launch the FastAPI agent
-echo "Starting StockAffirm API Server..."
-uvicorn main:app --host 0.0.0.0 --port 8501
+# Launch the agent
+echo "Starting StockAffirm Agent..."
+python main.py
